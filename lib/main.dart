bdbc686b641a434bcb3d368542ee1ce22af0e60e
Charlotte Isambert <charlotte.isambert@gmail.com>
2021-07-10 18:35:28 +0200

    (main): remove app title

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,8 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = 'Cardiac coherence';
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text(_title),
-            ),
-            body: Home()));
+    return MaterialApp(home: Scaffold(body: Home()));
   }
 }
