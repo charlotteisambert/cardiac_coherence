dd3a062337299679ec7136daccffcc2d440dac46
Charlotte Isambert <charlotte.isambert@gmail.com>
2021-07-17 13:11:09 +0200

    (colors): rename into more generic

--- lib/components/home/home.dart
+++ lib/components/home/home.dart
@@ -6,7 +6,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(color: Color(colors['blue'])),
+        decoration: BoxDecoration(color: Color(colors['background'])),
         child: BarsAnimationWrapper());
   }
 }
--- lib/utils/style.dart
+++ lib/utils/style.dart
@@ -1,4 +1,4 @@
 Map colors = Map.unmodifiable({
-  'blue': 0xFFF6E8E5,
+  'background': 0xFFF6E8E5,
   'light': 0xFFD4AF37,
 });
